51363e44f070974705208d65e58568a37039f58a
Түвшинжаргал <53771399+atuvshinjargal@users.noreply.github.com>
2022-08-20 18:09:40 +0800

    irisData

--- irisData.py
+++ irisData.py
@@ -9,37 +9,31 @@ data = load_iris()
 ############## Problem 1 ################
 # Data acquisition
 
-X = pd.DataFrame(data = data.data, columns = data.feature_names)
-y = pd.DataFrame(data = data.target, columns = ['species'])
-print(X.head())
-print(y.head())
+a = pd.DataFrame(data = data.data, columns = data.feature_names)
+b = pd.DataFrame(data = data.target, columns = ['species'])
+print(a.head())
+print(b.head())
 
-############## Problem 2 ################
 # Data combination
 
-df = pd.concat([X,y], axis=1)
+df = pd.concat([a,b], axis=1)
 print(df.head())
 
-############# Problem 3 #################
 # Confirmation of data
 
 print(df.info())
 print(df.describe())
-print('4th sample: {}'.format(df.loc[4]))
-print('total number of samples for each label: {}'.format(df.value_counts('species')))
+print('fourth sample: {}'.format(df.loc[4]))
+print('Count samples for each label: {}'.format(df.value_counts('sepal width (cm)')))
 count = np.count_nonzero(df.isnull())
 print('There is {} missing values.'.format(count))
 
-############ Problem 4, 5 #################
 # Investigation of the iris dataset itself
 
-# Extract columns 1st method using loc
+# get columns 1st method using loc
 sepal_width = df.loc[:, 'sepal width (cm)']
-# Extract columns 2nd method using iloc
+# get columns 2nd method using iloc
 sepal_width1 = df.iloc[:, 1]
-print("Main difference between loc and iloc is: \
-    loc gets rows (and/or columns) with labels. \
-    iloc gets rows (and/or columns) at integer locations.")
 
 sub_data = df.iloc[50:100,:]
 print(sub_data)
@@ -48,15 +42,14 @@ print(sub_data_petal_length)
 matched_df = df[df["petal width (cm)"] == 0.2]
 print(matched_df)
 
-############# Problem 6 #################
 # Creating a diagram
 
-#a = df.groupby('species').sum()
+a = df.groupby('species').sum()
 
 species_count = df['species'].value_counts(normalize=True)*100
 print("count value", species_count)
-#species_count1 = df.groupby('species').size()
-#print("count value", species_count1.shape)
+species_count1 = df.groupby('species').size()
+print("count value", species_count1.shape)
 
 #print("count", species_count.iloc[:,0])
 plt.pie(species_count, labels=['0 class', '1 class', '2 class'], autopct='%0.1f%%')
@@ -88,7 +81,6 @@ plot_violin(df_0, '0th')
 plot_violin(df_1, '1st')
 plot_violin(df_2, '2nd')
 
-############## Problem 7 ################
 # Confirmation of the relationship between features
 
 g = sns.pairplot(df, vars=["sepal width (cm)", "sepal length (cm)"], hue="species", kind='scatter')
@@ -114,6 +106,5 @@ plt.title('Correlation')
 plt.show()
 
 print('from the matrix and figures, I want to state few results: \
-    1. 0th class is very different from 1 and 2 class with all features. \
-    2. 1 and 2 classes are similar with sepal length and sepal width \
-    3. We can distinguish 1 and 2 classes with pedal length and pedal width.')
+    1. 1 and 2 classes are similar with sepal length and sepal width \
+    2. first class iris is very different from 1 and 2 class with all features.')
